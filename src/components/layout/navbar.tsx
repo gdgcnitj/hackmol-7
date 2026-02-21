8a5eb37a9ff97945f66d986e94e30258c30ff831
chahatkesh <ckesharwani4@gmail.com>
2026-02-21 16:16:25 +0530

    feat: enhance register button with decorative ornaments and improved styles

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -25,8 +25,38 @@ export default function Navbar() {
                 <a href="#sponsors">Sponsors</a>
                 <a href="#faq">FAQ</a>
             </div>
-            <div>
-                <button className="register-btn">Register</button>
+            <div className="register-btn-wrapper">
+                <button className="register-btn">
+                    <Image 
+                        src="/images/right-corner.png" 
+                        alt="" 
+                        width={30} 
+                        height={30}
+                        className="ornament ornament-top-left"
+                    />
+                    <Image 
+                        src="/images/right-corner.png" 
+                        alt="" 
+                        width={30} 
+                        height={30}
+                        className="ornament ornament-top-right"
+                    />
+                    <Image 
+                        src="/images/right-corner.png" 
+                        alt="" 
+                        width={30} 
+                        height={30}
+                        className="ornament ornament-bottom-left"
+                    />
+                    <Image 
+                        src="/images/right-corner.png" 
+                        alt="" 
+                        width={30} 
+                        height={30}
+                        className="ornament ornament-bottom-right"
+                    />
+                    <span className="btn-text">Register</span>
+                </button>
             </div>
         </nav>
     )
